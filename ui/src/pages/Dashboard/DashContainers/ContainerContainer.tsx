0c80ce096762a9291825f5e6da33ba708ac9cb76
Cristian Corrales <crisdevs95@gmail.com>
2023-06-26 20:56:51 -0400

    fixed buttons inside table

--- ui/src/pages/Dashboard/DashContainers/ContainerContainer.tsx
+++ ui/src/pages/Dashboard/DashContainers/ContainerContainer.tsx
@@ -2,7 +2,7 @@ import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 
 const rows : GridRowsProp = [
-    {id: 1, col1:"Container 1", col2:"20%", col3: "0.3", col4:"20%", col5: "20 min", col7: <button>Delete</button> },
+    {id: 1, col1:"Container 1", col2:"20%", col3: "0.3", col4:"20%", col5: "20 min", col7:" " },
     {id: 2, col1:"Container 2", col2:"20%", col3: "0.4", col4:"20%", col5: "20 min", col7: <button>Delete</button> },
     {id: 3, col1:"Container 3", col2:"20%", col3: "0.6", col4:"20%", col5: "20 min", col7: <button>Delete</button> },
     {id: 4, col1:"Container 4", col2:"20%", col3: "0.32", col4:"20%", col5: "20 min", col7: <button>Delete</button> },
@@ -15,7 +15,7 @@ const columns : GridColDef[] = [
     {field: "col4", headerName: "Memory", width: 150},
     {field: "col5", headerName: "Run Time", width: 150},
     // {field: "col6", headerName: "Image1 Conts", width: 150},
-    {field: "col7", headerName: "Container Options", width: 150}
+    {field: "col7", headerName: "Container Options", width: 150, renderCell: () =>{ return <button>Delete</button>}}
 ];
 
 
