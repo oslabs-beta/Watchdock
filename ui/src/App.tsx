98f165cbb01959f1e78b7d47e732effa945f4ba5
John Nevarez <nevarezjonathan11@gmail.com>
2023-09-21 16:48:53 -0400

    test

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,7 +4,7 @@ import ImageContainer from './pages/Dashboard/DashContainers/ImageContainer';
 import ContainerContainer from './pages/Dashboard/DashContainers/ContainerContainer';
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
 
-
+//This is my test comment
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
 const client = createDockerDesktopClient();
